cdbe241535c5e90d1fec5471f35e21890c5c2c3d
Artem <a.k.multiversity@gmail.com>
2020-03-16 09:22:46 +0300

    Fixed an error in the text.

--- src/test/java/by/rest/store/PetServiceTest.java
+++ src/test/java/by/rest/store/PetServiceTest.java
@@ -3,7 +3,7 @@ package by.rest.store;
 import by.rest.store.model.Category;
 import by.rest.store.model.Pet;
 import by.rest.store.service.PetService;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,20 +15,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class PetServiceTest {
-//     private PetService petService = new PetService();
-//     private Pet pet = new Pet(
-//             0,
-//             new Category(0, "test"),
-//             "test",
-//             new ArrayList<>(),
-//             new ArrayList<>(),
-//             Pet.Status.available
-//     );
+     private PetService petService;
+     private Pet pet;
      
-     PetService petService;
-     Pet pet;
-     
-     @Before
+     @BeforeEach
      public void createNewPet() {
           petService = new PetService();
           pet = new Pet(
@@ -44,7 +34,6 @@ public class PetServiceTest {
      
      @Test
      public void getPetsList() {
-//          petService.addPet(pet);
           Map<Long, Pet> expected = petService.getPetsList();
           
           Map<Long, Pet> actual = new HashMap<>();
